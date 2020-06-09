18224770c2e46e6531e6b88f0b81281579463bce
3pyp <gcristian@google.com>
2020-06-09 18:47:46 -0500

    Policy added

--- models/User.js
+++ models/User.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  policy: {
+    type: Boolean,
+    required: true
+  },
   phone_number: Number,
   born_date: String,
   profile_img: String,
